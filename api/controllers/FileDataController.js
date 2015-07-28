41856458439e78a03d81513725c6ba7cf369f3df
vortigern <mickael.lecabellec@gmail.com>
2015-07-28 20:05:22 +0200

    Small changes

---
+++ api/controllers/FileDataController.js
@@ -0,0 +1,41 @@
+/**
+ * FileDataController
+ *
+ * @description :: Server-side logic for managing filedatas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `FileDataController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `FileDataController.upload()`
+   */
+  upload: function (req, res) {
+    return res.json({
+      todo: 'upload() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `FileDataController.get()`
+   */
+  get: function (req, res) {
+    return res.json({
+      todo: 'get() is not implemented yet!'
+    });
+  }
+};
+
--- api/models/AppUser.js
+++ api/models/AppUser.js
@@ -1,18 +1,62 @@
 /**
-* AppUser.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * AppUser.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
   attributes: {
 
-    username : { type: 'string', unique: true , size:64 },
+    username: {
+      type: 'string',
+      unique: true,
+      size: 64,
+      required: true,
+      primaryKey: true
+    },
 
-    secret1 : { type: 'string' , size:254 },
+    secret1: {
+      type: 'string',
+      size: 254
+    },
 
-    secret2 : { type: 'string' , size: 254}
+    secret2: {
+      type: 'string',
+      size: 254
+    },
+
+    creationdate: {
+      type: 'datetime'
+    },
+
+    locked: {
+      type: 'binary',
+      defaultsTo: false
+    },
+
+    activated: {
+      type: 'binary',
+      defaultsTo: false
+    },
+
+    superadmin: {
+      type: 'binary',
+      defaultsTo: false
+    },
+
+    token: {
+      type: 'string',
+      size: 254,
+      unique: true
+    },
+
+    userEmail: {
+      type: 'email',
+      size: 254,
+      unique: true
+    }
+
   }
 };
