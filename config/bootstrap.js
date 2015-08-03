de178fd4da9bbee03a8cb2e6293f368e99833b2a
vortigern <mickael.lecabellec@gmail.com>
2015-08-03 16:18:26 +0200

    +bootstrap copied from chaton

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,5 +13,104 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+  
+ 
+
+    var cryptoJS = require("crypto-js");
+    var bootstrappedUsers = [{username: "root", password: cryptoJS.SHA256("root").toString(cryptoJS.enc.Base64), isActive: true, isAdmin: true, email: "mickael.lecabellec@gmail.com"},
+        {username: "test1", password: cryptoJS.SHA256("test1").toString(cryptoJS.enc.Base64), isActive: true, isAdmin: false, email: "test1@booleanworks.com"},
+        {username: "test2", password: cryptoJS.SHA256("test2").toString(cryptoJS.enc.Base64), isActive: true, isAdmin: false, email: "test2@booleanworks.com"},
+        {username: "test3", password: cryptoJS.SHA256("test3").toString(cryptoJS.enc.Base64), isActive: true, isAdmin: false, email: "test3@booleanworks.com"}];
+
+    bootstrappedUsers.forEach(function (cUser, cb) {
+        AppUser.findOrCreate(cUser, cUser).exec(function (err, cUser) {
+            if (err !== null)
+            {
+                console.log("Error with user creation/check: " + err);
+            } else
+            {
+                console.log("User found or created: " + JSON.stringify(cUser));
+
+            }
+
+            var relatedProfile = {user: cUser, displayName: cUser.username};
+            console.log("relatedProfile: " + JSON.stringify(relatedProfile));
+            
+            UserProfile.findOrCreate({displayName: cUser.username}, relatedProfile).exec(function (err, cUserProfile) {
+                if (err !== null)
+                {
+                    console.log("Error with user profile creation/check: " + err);
+                    console.log("Error with user profile creation/check, profile: " + JSON.stringify(cUserProfile));
+                } else
+                {
+                    console.log("User profile found or created: " + JSON.stringify(cUserProfile));
+
+
+                    if (cUser.username === "root")
+                    {
+                        var serverStatusThread = {title: "Server status thread"};
+
+                        UserThread.findOrCreate(serverStatusThread, serverStatusThread).exec(function (err, cThread) {
+
+                            if (err !== null)
+                            {
+                                console.log("Error with server status thread creation/check: " + err);
+                            } else
+                            {
+                                console.log("Server status thread found or created: " + JSON.stringify(cThread));
+
+                                if (err !== null)
+                                {
+                                    console.log("Error with root's profile retrieval: " + err);
+                                } else
+                                {
+
+                                    console.log("Root's profile found: " + JSON.stringify(cUserProfile));
+
+                                    var startupMessage = {thread: cThread.id, userProfile: cUserProfile.id, content: "Server started at " + new Date().toISOString()};
+                                    UserMessage.findOrCreate(startupMessage, startupMessage).exec(function (err, cMessage) {
+                                        if (err !== null)
+                                        {
+                                            console.log("Error with server status message creation/check: " + err);
+                                        } else
+                                        {
+                                            if (cMessage === undefined)
+                                            {
+                                                console.log("Server status message NOT created: " + cMessage);
+                                            } else
+                                            {
+                                                console.log("Server status message created: " + JSON.stringify(cMessage));
+
+                                            }
+
+                                        }
+
+
+                                    });
+                                }
+
+
+
+                            }
+
+
+
+
+
+
+
+                        });
+                    }
+
+                }
+
+            });
+
+        });
+    });
+ 
+  
+  
+  
   cb();
 };
