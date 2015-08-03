bf8c13aa1c6608bd7d4044fff79b6c26ec856960
vortigern <mickael.lecabellec@gmail.com>
2015-08-03 16:23:58 +0200

    +Polocies copied from chaton

--- config/policies.js
+++ config/policies.js
@@ -26,7 +26,16 @@ module.exports.policies = {
   *                                                                          *
   ***************************************************************************/
 
-  // '*': true,
+    '*': false,
+    UserMessageController: {
+        "*": "multiAuth1"
+    },
+    AuthenticationController: {
+        "*": true
+    },
+    UserThreadController: {
+        "*": "multiAuth1"
+    }
 
   /***************************************************************************
   *                                                                          *
