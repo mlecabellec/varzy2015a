61550c62cd22cb7f9d432a4e615d2208742904d2
vortigern <mickael.lecabellec@gmail.com>
2015-08-04 17:53:49 +0200

    +WIP around templates and i18n.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,60 @@
  * and open the template in the editor.
  */
 
+//Require JST, lodash and JQuery
 
-var app = {
-    
-    
-}
+
+this["APP"] = this["APP"] || {};
+
+this.APP["login"] = this.APP["login"] || {
+    gui: {
+        loginUrl: "",
+        loginModal:
+                {
+                    openModal: function (i18n) {
+
+                        $("body").append(JST["assets/templates/login.html"](
+                                {i18n:i18n}
+                        ));
+                
+                        //TODO: wire events for login
+                
+                        $("#loginModal").modal();
+                    },
+                    closeModal: function () 
+                    {
+                        //TODO: remove this
+                        
+                        //$("#loginModal").modal("hide");
+                        //$("#loginModal").remove();
+                        
+                    },
+                    doJsonLogin: function () {
+                    },
+                    jsonLoginCb: function () {
+                    },
+                    doJsonLogout: function () {
+                    },
+                    jsonLogoutCb: function () {
+                    }
+                },
+        helpers:
+                {
+                    checkAuth: function () {
+                    }
+                }
+    }
+};
+
+
+this.APP["views"] = this.APP["views"] || {
+    home: {init: function () {
+        }
+    },
+    test001: {init: function () {
+        }
+    }
+};
+
+this.APP["debug"] = this.APP["debug"] || {};
+this.APP["logs"] = this.APP["logs"] || {};
