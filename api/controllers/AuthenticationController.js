37de5f1d89e88263c241366cee3151558f7e50df
vortigern <mickael.lecabellec@gmail.com>
2015-08-18 11:43:30 +0200

    + implementing a "ticket logic" in order to enforce security

--- api/controllers/AuthenticationController.js
+++ api/controllers/AuthenticationController.js
@@ -5,179 +5,181 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
- var cryptoJS = require("crypto-js");
+var cryptoJS = require("crypto-js");
+var uuid = require('node-uuid');
+var forge = require('node-forge');
 
- module.exports = {
-     /**
-      * `AuthenticationController.index()`
-      */
-     index: function (req, res) {
-         return res.json({
-             todo: 'index() is not implemented yet!'
-         });
-     },
-     /**
-      * `AuthenticationController.login()`
-      */
-     login: function (req, res) {
+module.exports = {
+    /**
+     * `AuthenticationController.index()`
+     */
+    index: function (req, res) {
+        return res.json({
+            todo: 'index() is not implemented yet!'
+        });
+    },
+    /**
+     * `AuthenticationController.login()`
+     */
+    login: function (req, res) {
 
-         var givenUsername = req.param("username");
-         var givenPassword = req.param("password");
+        var givenUsername = req.param("username");
+        var givenPassword = req.param("password");
 
-         if (givenUsername === null || givenUsername === undefined)
-         {
-             return res.json({
-                 message: "Bad username.",
-                 errorMessage: "Bad username. No valid username parameter found",
-                 code: 3000
-             });
-         }
+        if (givenUsername === null || givenUsername === undefined)
+        {
+            return res.json({
+                message: "Bad username.",
+                errorMessage: "Bad username. No valid username parameter found",
+                code: 3000
+            });
+        }
 
-         if (givenPassword === null || givenPassword === undefined)
-         {
-             return res.json({
-                 message: "Bad password.",
-                 errorMessage: "Bad password. No valid password parameter found",
-                 code: 3010
-             });
-         }
-
-
-         var lookingForUser = {username: givenUsername, password: cryptoJS.SHA256(givenPassword).toString(cryptoJS.enc.Base64), isActive: true};
-
-         AppUser.findOne(lookingForUser, function (err, cUser) {
-
-             if (err !== null)
-             {
-                 console.log("login error: " + err);
-                 return res.json({
-                     message: "Login problem.",
-                     errorMessage: "Login problem. Please check this error: " + err,
-                     code: 3020,
-                     err: err
-                 });
-             }
-
-             if (cUser !== undefined)
-             {
-                 console.log("login, user found: " + cUser.username);
-                 req.session.authenticated = true;
-                 req.session.sessionkey = cUser.sessionkey;
-                 req.session.username = cUser.username;
-
-                 //req.signedCookies.authenticated = true;
-                 //req.signedCookies.sessionkey = cUser.sessionkey;
-                 //req.signedCookies.username = cUser.username;
-
-                 res.cookie('authenticated', true);
-                 res.cookie('sessionkey', cUser.sessionkey);
-                 res.cookie('username', cUser.username);
+        if (givenPassword === null || givenPassword === undefined)
+        {
+            return res.json({
+                message: "Bad password.",
+                errorMessage: "Bad password. No valid password parameter found",
+                code: 3010
+            });
+        }
 
 
-                 console.log("login, req.session.authenticated: " + req.session.authenticated);
-                 console.log("login, req.session.authenticated: " + req.session.authenticated);
-                 console.log("login, req.session.sessionkey: " + req.session.sessionkey);
-                 console.log("login, req.session.username: " + req.session.username);
+        var lookingForUser = {username: givenUsername, password: cryptoJS.SHA256(givenPassword).toString(cryptoJS.enc.Base64), isActive: true};
 
-                 console.log("login, req.cookies.authenticated: " + req.cookies.authenticated);
-                 console.log("login, req.cookies.sessionkey: " + req.cookies.sessionkey);
-                 console.log("login, req.cookies.username: " + req.cookies.username);
+        AppUser.findOne(lookingForUser, function (err, cUser) {
 
-                 return res.json({
-                     message: "Login OK.",
-                     errorMessage: "",
-                     code: 0,
-                     username:cUser.username,
-                     authenticated:true
-                 });
+            if (err !== null)
+            {
+                console.log("login error: " + err);
+                return res.json({
+                    message: "Login problem.",
+                    errorMessage: "Login problem. Please check this error: " + err,
+                    code: 3020,
+                    err: err
+                });
+            }
 
-             } else
-             {
-                 console.log("login, problem with user: " + cUser);
-                 return res.json({
-                     message: "Login problem.",
-                     errorMessage: "Login problem. Please check this error: " + err,
-                     code: 3040,
-                     err: err
-                 });
-             }
+            if (cUser !== undefined)
+            {
+                console.log("login, user found: " + cUser.username);
+                req.session.authenticated = true;
+                req.session.sessionkey = cUser.sessionkey;
+                req.session.username = cUser.username;
 
-         });
+                //req.signedCookies.authenticated = true;
+                //req.signedCookies.sessionkey = cUser.sessionkey;
+                //req.signedCookies.username = cUser.username;
 
-
-     },
-     /**
-      * `AuthenticationController.logout()`
-      */
-     logout: function (req, res) {
-
-         res.clearCookie('authenticated');
-         res.clearCookie('sessionkey');
-         res.clearCookie('username');
-
-         req.session.authenticated = true;
-         req.session.sessionkey = "";
-         req.session.username = "";
+                res.cookie('authenticated', true);
+                res.cookie('sessionkey', cUser.sessionkey);
+                res.cookie('username', cUser.username);
 
 
-         return res.json({
-             message: "Logged out. Goodbye !",
-             errorMessage: "",
-             code: 0
-         });
-     },
-     /**
-      * `AuthenticationController.register()`
-      */
-     register: function (req, res) {
+                console.log("login, req.session.authenticated: " + req.session.authenticated);
+                console.log("login, req.session.authenticated: " + req.session.authenticated);
+                console.log("login, req.session.sessionkey: " + req.session.sessionkey);
+                console.log("login, req.session.username: " + req.session.username);
 
-         res.clearCookie('authenticated');
-         res.clearCookie('sessionkey');
-         res.clearCookie('username');
+                console.log("login, req.cookies.authenticated: " + req.cookies.authenticated);
+                console.log("login, req.cookies.sessionkey: " + req.cookies.sessionkey);
+                console.log("login, req.cookies.username: " + req.cookies.username);
 
-         req.session.authenticated = true;
-         req.session.sessionkey = "";
-         req.session.username = "";
+                return res.json({
+                    message: "Login OK.",
+                    errorMessage: "",
+                    code: 0,
+                    username: cUser.username,
+                    authenticated: true
+                });
 
-         var givenUsername = req.param("username");
-         var givenPassword1 = req.param("password1");
-         var givenPassword2 = req.param("password2");
-         var givenEmail = req.param("email");
+            } else
+            {
+                console.log("login, problem with user: " + cUser);
+                return res.json({
+                    message: "Login problem.",
+                    errorMessage: "Login problem. Please check this error: " + err,
+                    code: 3040,
+                    err: err
+                });
+            }
+
+        });
 
 
+    },
+    /**
+     * `AuthenticationController.logout()`
+     */
+    logout: function (req, res) {
 
-         var newUser = {username: givenUsername, password: cryptoJS.SHA256(givenPassword1).toString(cryptoJS.enc.Base64), isActive: true, isAdmin: false, email: givenEmail};
+        res.clearCookie('authenticated');
+        res.clearCookie('sessionkey');
+        res.clearCookie('username');
 
-         AppUser.create(newUser).exec(function (err, record) {
+        req.session.authenticated = true;
+        req.session.sessionkey = "";
+        req.session.username = "";
 
-             if (err !== null)
-             {
-                 console.log("Registration error: " + err);
-                 return res.json({
-                     message: "Registration problem.",
-                     errorMessage: "Registration problem. Please check this error: " + err,
-                     code: 3080,
-                     err: err
-                 });
-             } else
-             {
-                 return res.json({
-                     message: "Registration OK.",
-                     errorMessage: "",
-                     code: 0,
-                     err: err
-                 });
-             }
+
+        return res.json({
+            message: "Logged out. Goodbye !",
+            errorMessage: "",
+            code: 0
+        });
+    },
+    /**
+     * `AuthenticationController.register()`
+     */
+    register: function (req, res) {
+
+        res.clearCookie('authenticated');
+        res.clearCookie('sessionkey');
+        res.clearCookie('username');
+
+        req.session.authenticated = true;
+        req.session.sessionkey = "";
+        req.session.username = "";
+
+        var givenUsername = req.param("username");
+        var givenPassword1 = req.param("password1");
+        var givenPassword2 = req.param("password2");
+        var givenEmail = req.param("email");
 
 
 
-         });
+        var newUser = {username: givenUsername, password: cryptoJS.SHA256(givenPassword1).toString(cryptoJS.enc.Base64), isActive: true, isAdmin: false, email: givenEmail};
 
-     },
-     /**
-      * `AuthenticationController.check()`
-      */
-     check: function (req, res) {
+        AppUser.create(newUser).exec(function (err, record) {
+
+            if (err !== null)
+            {
+                console.log("Registration error: " + err);
+                return res.json({
+                    message: "Registration problem.",
+                    errorMessage: "Registration problem. Please check this error: " + err,
+                    code: 3080,
+                    err: err
+                });
+            } else
+            {
+                return res.json({
+                    message: "Registration OK.",
+                    errorMessage: "",
+                    code: 0,
+                    err: err
+                });
+            }
+
+
+
+        });
+
+    },
+    /**
+     * `AuthenticationController.check()`
+     */
+    check: function (req, res) {
 
         var authData = {
             hasCoockie: false,
@@ -340,10 +342,14 @@
 
             //return res.forbidden('You are not permitted to perform this action.');
         }
-        
+
         return res.json(authData);
 
 
 
-     }
- };
+    },
+    getticket: function (req, res)
+    {
+
+    }
+};
